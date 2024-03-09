99a29be67402e3492a494a165ef6876f3d05d24d
DmitryRudinsky <guladmitriy1331@gmail.com>
2024-03-09 22:53:25 +0300

    second version of modal window

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import './index.scss'
+import './styles/index.scss'
+import Modal from './components/modal';
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -7,15 +8,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => setOpen(true)} className='open-modal-btn'>✨ Открыть окно</button>
-      {open && <div className="overlay">
-        <div className="modal">
-          <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div> }
+      <Modal open={open} setOpen={setOpen}/>
     </div>
   );
 }
